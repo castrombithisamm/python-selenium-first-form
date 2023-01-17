c02b7d7cea7f2356c3222011c7382cfeae95ce5c
castrombithisamm <castrombithi@live.com>
2023-01-17 10:22:13 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options)
+driver.get("https://jqueryui.com/resources/demos/progressbar/download.html")
+driver.implicitly_wait(8)
+# progress_element = driver.find_element("class name", "progress-label")
+# print(f"{progress_element.text == 'Completed!'}")
