4edfcfac5f4f75b190dee188b1cb1a22d100fc7f
castrombithisamm <castrombithi@live.com>
2023-01-17 10:39:35 +0300

    add selenium easy basic first form demo link

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from selenium.webdriver.chrome.options import Options
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
-driver.get("https://jqueryui.com/resources/demos/progressbar/download.html")
+driver.get("https://demo.seleniumeasy.com/basic-first-form-demo.html")
 driver.implicitly_wait(8)
 # progress_element = driver.find_element("class name", "progress-label")
 # print(f"{progress_element.text == 'Completed!'}")
