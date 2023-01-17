9265f549cbacae7216c385fb8c6fc630e3f160c7
castrombithisamm <castrombithi@live.com>
2023-01-17 11:37:42 +0300

    add keys to calculate sum, add try except code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 
 
 
@@ -9,6 +10,16 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
 driver.get("https://demo.seleniumeasy.com/basic-first-form-demo.html")
-driver.implicitly_wait(8)
-# progress_element = driver.find_element("class name", "progress-label")
-# print(f"{progress_element.text == 'Completed!'}")
+driver.implicitly_wait(5)
+
+try:
+    no_button = driver.find_element("class name", "at-cm-no-button")
+    no_button()
+except:
+    print("No element with this class name. Skipping ...")
+
+sum1 = driver.find_element("id", "sum1")
+sum2 = driver.find_element("id", "sum2")
+
+sum1.send_keys(15)
+sum2.send_keys(15)
